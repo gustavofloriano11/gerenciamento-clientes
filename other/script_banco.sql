0ed2b3db7e0f152026c25f2bc5ff3762ac9e085b
gustavofloriano11 <gustavo_floriano1@estudante.senai.org.br>
2024-11-22 20:08:17 -0300

    fix: arrumar nome das colunas do banco

--- other/script_banco.sql
+++ other/script_banco.sql
@@ -6,7 +6,7 @@ USE gerenciamento_clientes;
 
 CREATE TABLE cliente(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
+    nome_cliente VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     telefone CHAR(11) NOT NULL,
     CPF CHAR(11) NOT NULL
@@ -14,7 +14,7 @@ CREATE TABLE cliente(
 
 CREATE TABLE funcionario(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(255) NOT NULL,
+	nome_funcionario VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     telefone CHAR(11) NOT NULL
 );
@@ -25,9 +25,8 @@ CREATE TABLE solicitacao(
     id_funcionario INT, 
     urgencia VARCHAR(5) NOT NULL,
     descricao TEXT NOT NULL,
-    status_gerenciamento VARCHAR(12) NOT NULL,
+    status_solicitacao VARCHAR(12) NOT NULL,
     data_abertura DATE,
     FOREIGN KEY (id_cliente) REFERENCES cliente (id),
     FOREIGN KEY (id_funcionario) REFERENCES funcionario (id)
-    
 );
